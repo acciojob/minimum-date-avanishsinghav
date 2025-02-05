5fbce2a02589d20c18b71bca3a1ebef310d1b9a5
Avanish Singh <107432071+avanishsinghav@users.noreply.github.com>
2025-02-05 10:35:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function minDate(dates) {
   //write you code here
+	return dates.reduce((earliest, currentDate)=>
+		currentDate < earliest ? currentDate : earliest;
+		)
 }
 
 // Do not change the code
@@ -36,5 +39,4 @@ var dates = [
   "2023/03/29",
   "2023/03/30",
 ];
-
 alert(minDate(dates));
